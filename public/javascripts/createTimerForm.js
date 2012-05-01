63109110d304bf5b0747cb7429484269f928d5a4
Emu Mantoid <stimularity@gmail.com>
2012-05-01 17:02:03 -0400

    Made the timer creation form way more sexy. (Ripped off Ivans code.)

--- public/javascripts/createTimerForm.js
+++ public/javascripts/createTimerForm.js
@@ -6,15 +6,24 @@ $(document).ready(function() {
 	
 	//Submit button
 	$("#timerformsubmit").click(function() {
+		submitForm();
+	});//
 
-		
+	//Submit by hitting enter
+	$('#timername').keyup(function(event) {
+		if((event.keyCode || event.which) == '13'){
+			submitForm();
+		}
+	});
+
+	function submitForm(){
 		if(seconds){ runtime = runtime/60; }
 
 		var t = new Timer(runtime); //Create a new timer object. This is the ONLY place where this happens
 		t.comment = $('#timername').val();
 
 		//check if min or sec
-		if(minutes == true && seconds == false){
+		if(minutes === true && seconds === false){
 			//minutes
 			t.type = 1;
 		}else{
@@ -29,17 +38,23 @@ $(document).ready(function() {
 			}
 			//alert("Data Loaded: " + data);
 		});
-			
-	});//
-	
-	/* var Timerschema = new Schema({ //Ref
-		start		:Date,
-		end			:Date,
-		repeat		:Number,
-		interval	:Number,
-		type		:Number,
-		comment		:String,
-	}); */
+	}
+
+	//Make that title box look damn pretty
+	$('#timername').focus(function(srcc) {
+		if($(this).val() == $(this)[0].title) {
+			$(this).removeClass("defaultTextActive");
+			$(this).val("");
+		}
+	});
+
+	$('#timername').blur(function() {
+		if($(this).val() === "") {
+			$(this).addClass("defaultTextActive");
+			$(this).val($(this)[0].title);
+		}
+	});
+	$('#timername').blur();
 	
 	//Timer window and duration adjustment buttons
 	$("#durationminus").click(function() {
@@ -59,14 +74,14 @@ $(document).ready(function() {
 	
 	//Minutes and seconds buttons.
 	$("#durationminutes").click(function() {
-			$("#durationminutes").css('background','#09F'); //Select
-			$("#durationseconds").css('background','#FFF'); //DeSelect
+			$("#durationminutes").css('background','url(/images/buttonbg.png) repeat-x top #09F'); //Select
+			$("#durationseconds").css('background','url(/images/buttonbg.png) repeat-x top #FFF'); //DeSelect
 			minutes = true;
 			seconds = false;
 	});
 	$("#durationseconds").click(function() {
-			$("#durationseconds").css('background','#09F'); //Select
-			$("#durationminutes").css('background','#FFF'); //Select
+			$("#durationseconds").css('background','url(/images/buttonbg.png) repeat-x top #09F'); //Select
+			$("#durationminutes").css('background','url(/images/buttonbg.png) repeat-x top #FFF'); //Select
 			seconds = true;
 			minutes = false;
 	});
