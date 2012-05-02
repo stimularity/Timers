9569e6efc28f0f4254d14205d2cbe7342e14dfac
Ivan Rudovol <ivan.rudovol@gmail.com>
2012-05-02 00:43:50 -0300

    Update routes/UserControl.js

--- routes/UserControl.js
+++ routes/UserControl.js
@@ -27,7 +27,7 @@ exports.login = function(req, res){
 
 	}
 
-  res.render('loginregister', { title: 'Login', name: 'Not Logged In' })
+  res.render('loginregister', { title: 'New to MoodTimer? Sign up', name: 'Not Logged In' })
 };
 
 exports.logout = function(req, res){
