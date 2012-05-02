385590f6fa87b6049524b3319de4bee5a526fec0
Ivan Rudovol <ivan.rudovol@gmail.com>
2012-05-02 00:40:36 -0300

    Update routes/UserControl.js

--- routes/UserControl.js
+++ routes/UserControl.js
@@ -27,7 +27,7 @@ exports.login = function(req, res){
 
 	}
 
-  res.render('loginregister', { title: 'Login/Register', name: 'Not Logged In' })
+  res.render('loginregister', { title: 'Login', name: 'Not Logged In' })
 };
 
 exports.logout = function(req, res){
